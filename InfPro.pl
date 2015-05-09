e8894268e3216101e0f41d7df3dab1150bfad614
ezequiel-de-valais <ezequiel.devalais@gmail.com>
2015-05-09 18:32:35 -0300

    actualizo inf pro.pl

--- InfPro.pl
+++ InfPro.pl
@@ -4,50 +4,25 @@ use Getopt::Long;
 use constant false => 0;
 use constant true  => 1;
 
-=pod
-	$PROCDIR = $ENV{'PROCDIR'};
-	$MAEDIR = $ENV{'MAEDIR'};
-	$INFODIR = $ENV{'INFODIR'};
-	$GRUPO = $ENV{'GRUPO'};
-
-	print "$GRUPO$PROCDIR\n";
-	print "$GRUPO$MAEDIR\n";
-	print "$GRUPO$INFODIR\n";
-
-	asignacion de variables
-	my $PROCDIRCOMPLETO = $GRUPO.$PROCDIR;
-	print "$PROCDIRCOMPLETO\n";
-
-	my $MAEDIRCOMPLETO = $GRUPO.$MAEDIR;
-	print "$MAEDIRCOMPLETO\n";
-
-	my $INFODIRCOMPLETO = $GRUPO.$INFODIR;
-	print "$INFODIRCOMPLETO\n";
-=cut
-
-##################################################
-
-#indice para verificar el valor del argumento
-my $j=0; 
-my $directorio;
-my $archivo;
-##################################################
-
-#Procesamiento de parametros de linea de comando
 main();
 
 
-
-##################################################
-    
-
 sub ayuda{
-    imprimir("TEXTO DE AYUDA");
+	imprimir("InfPro: El propósito de este comando es resolver consultas sobre los documentos protocolizados y emitir informes y estadisticas sobre ellos.");
+	imprimir("Opciones:");
+	imprimir("\ta:\t\tMostar ayuda.");
+	imprimir("\tc:\t\tConsultar.");
+	imprimir("\ti:\t\tInforme.");
+	imprimir("\te:\t\tEstadisticas.");
+	imprimir("\tgc:\t\tConsultar y guardar.");
+	imprimir("\tgi:\t\tInforme y guardar.");
+	imprimir("\tge:\t\tEstadisticas y guardar.");
+	imprimir("\ts:\t\tSalir.");
 }
 sub imprimir {
     $a = shift."\n";
     print $a;
-};
+}
 
 sub cantidadDeParametros{
 	$cantidad = $#ARGV;
@@ -60,7 +35,6 @@ sub parametrosValidos{
 		imprimir("todo mal");
 		return false;
 	}
-
 	imprimir("todo bien");
 	return true;
 	
@@ -68,7 +42,7 @@ sub parametrosValidos{
 
 # Valida que el ambiente se haya inicializado con el IniPro.sh
 
-sub validarAmbienteInicializado{
+sub esAmbienteValido{
    my $PROCDIR = $ENV{'PROCDIR'};
    my $MAEDIR = $ENV{'MAEDIR'};
    my $INFODIR = $ENV{'INFODIR'};
@@ -76,78 +50,113 @@ sub validarAmbienteInicializado{
 
    if ( ! defined $GRUPO || $GRUPO eq "" || ! defined $PROCDIR || $PROCDIR eq "" || ! defined $MAEDIR || $MAEDIR eq "" || ! defined $INFODIR || $INFODIR eq "" )
    {
-      print "Ambiente no inicializado. Ejecute el comando . IniPro.sh\n";
-      return 1;
+      imprimir("Ambiente no inicializado. Ejecute el comando . IniPro.sh");
+      return false;
    }
-   return 0;
+   return true;
 
 }
 
 # Valida que no esté corriendo alguna instancia de InfPro
 
-sub validarInfProNoEsteCorriendo{
+sub estaInfProCorriendo{
 
    my $InfProPIDCant=`ps ax | grep -v "grep" | grep -v "gedit" | grep -o "InfPro" | sed 's-\(^ *\)\([0-9]*\)\(.*\$\)-\2-g' | wc -l`;
    if ($InfProPIDCant > 1)
    {
-      print "Ya se está ejecutando el programa InfPro. Por favor, espere a que el mismo termine.\n";
-      return 1;
+      imprimir("Ya se está ejecutando el programa InfPro. Por favor, espere a que el mismo termine.");
+      return false;
    }
-   return 0;
+   return true;
 
 }
 
+sub mostrarMenuPrencipal{
+	imprimir ("MENU PRINCIPAL");
+}
 
-sub logger{
-	system("Glog.sh perl log INFO");
+##ACA HAY QUE SETEAR DONDE SIGUE CORRIENDO DESPUES DE ELEGIR UNA OPCION
+sub levantarProcesoOpcion{
+
+	#primer parametro debe ser opciones
+	$opcion = shift;
+	if($opcion eq "a"){
+		imprimir("parametro a");
+		ayuda();
+	}
+
+	if($opcion eq "c"){
+		imprimir("parametro c");
+		consulta(true);
+	}
+	if($opcion eq "gc"){
+		imprimir("parametro gc");
+		consulta(false);
+	}
+	if($opcion eq "e"){
+		imprimir("parametro e");
+	}
+	if($opcion eq "ge"){
+		imprimir("parametro ge");
+	}
+	if($opcion eq "gi"){
+		imprimir("parametro gi");
+	}
+	if($opcion eq "i"){
+		imprimir("parametro i");
+	}
+	if($opcion eq "s"){
+		imprimir("cerrando InfPro");
+		exit 1;
+	}
 }
 
 sub main{
-	logger();
-   	if ( validarAmbienteInicializado() == 0)
-   	{
-      		if ( validarInfProNoEsteCorriendo() == 0 )
-       		{
-			if (!parametrosValidos()) {
-				imprimir("algo salio mal");
-				ayuda();
-				exit;
-			}
+   	if ( !esAmbienteValido()){
+   		exit 1;
+   	}
 
-			$parametroValido = false;
-			#primer parametro debe ser opciones
-			$opcion = $ARGV[0];
-			if($opcion eq "-a"){
-				imprimir("parametro -a");
-				ayuda();
-				$parametroValido = true;
-			}
-			if($opcion eq "-c"){
-				imprimir("parametro -c");
-				$parametroValido = true;
-			}
-			if($opcion eq "-i"){
-				imprimir("parametro -i");
-				$parametroValido = true;
-			}
-			if($opcion eq "-e"){
-				imprimir("parametro -e");
-				$parametroValido = true;
-			}
-			if($opcion eq "-cg"){
-				imprimir("parametro -cg");
-				$parametroValido = true;
-			}
-			if($opcion eq "-ig"){
-				imprimir("parametro -ig");
-				$parametroValido = true;
-			}
-			if($opcion eq "-eg"){
-				imprimir("parametro -eg");
-				$parametroValido = true;
-			}
-	
-		}
+	if ( !estaInfProCorriendo() ){
+		exit 1;
 	}
-	
+			
+	#mostrarMenuPrencipal();
+	while(true){
+		menuPrincipal();
+	}
 }
+
+sub in_array {
+  my ($item, $array) = @_;
+  my %hash = map { $_ => 1 } @$array;
+  if ($hash{$item}) { return 1; } else { return 0; }
+}
+
+sub menuPrincipal{
+	ayuda();
+	imprimir("Ingrese una opción: ");
+	$entrada = <STDIN>;
+	chomp($entrada);	
+	
+	#Loop infinito hasta que se ingrese opcion valida
+	my @entradas=("a","c","gc","i","gi","e","ge","s", "");
+	while(!in_array($entrada,\@entradas) ){
+		imprimir("Opcion incorrecta. Intente nuevamente: ");
+		$entrada = <STDIN>;
+		chomp($entrada);
+	}
+	levantarProcesoOpcion($entrada);
+	return $entrada;
+}
+
+sub consulta{
+	$conGuardar= shift;
+	imprimir("CONSULTANDOOOOOOOOOO");
+	if($conGuardar){
+		imprimir("guardando");
+	}else{
+		imprimir("sin Guardar");
+	}
+}
+
+
