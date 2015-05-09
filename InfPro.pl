48cec4859aa005e93e14745c3476446958a5bbde
ezequiel-de-valais <ezequiel.devalais@gmail.com>
2015-05-09 13:41:30 -0300

    validar opciones

--- InfPro.pl
+++ InfPro.pl
@@ -1,20 +1,118 @@
 #!/usr/bin/perl -w
+use Getopt::Long;
 
-$PROCDIR = $ENV{'PROCDIR'};
-$MAEDIR = $ENV{'MAEDIR'};
-$INFODIR = $ENV{'INFODIR'};
-$GRUPO = $ENV{'GRUPO'};
+use constant false => 0;
+use constant true  => 1;
 
-print "$GRUPO$PROCDIR\n";
-print "$GRUPO$MAEDIR\n";
-print "$GRUPO$INFODIR\n";
+=pod
+	$PROCDIR = $ENV{'PROCDIR'};
+	$MAEDIR = $ENV{'MAEDIR'};
+	$INFODIR = $ENV{'INFODIR'};
+	$GRUPO = $ENV{'GRUPO'};
 
-#asignacion de variables
-my $PROCDIRCOMPLETO = $GRUPO.$PROCDIR;
-print "$PROCDIRCOMPLETO\n";
+	print "$GRUPO$PROCDIR\n";
+	print "$GRUPO$MAEDIR\n";
+	print "$GRUPO$INFODIR\n";
 
-my $MAEDIRCOMPLETO = $GRUPO.$MAEDIR;
-print "$MAEDIRCOMPLETO\n";
+	asignacion de variables
+	my $PROCDIRCOMPLETO = $GRUPO.$PROCDIR;
+	print "$PROCDIRCOMPLETO\n";
 
-my $INFODIRCOMPLETO = $GRUPO.$INFODIR;
-print "$INFODIRCOMPLETO\n";
+	my $MAEDIRCOMPLETO = $GRUPO.$MAEDIR;
+	print "$MAEDIRCOMPLETO\n";
+
+	my $INFODIRCOMPLETO = $GRUPO.$INFODIR;
+	print "$INFODIRCOMPLETO\n";
+=cut
+
+##################################################
+
+#indice para verificar el valor del argumento
+my $j=0; 
+my $directorio;
+my $archivo;
+##################################################
+
+#Procesamiento de parametros de linea de comando
+main();
+
+
+
+##################################################
+    
+
+sub ayuda{
+    imprimir("TEXTO DE AYUDA");
+}
+sub imprimir {
+    $a = shift."\n";
+    print $a;
+};
+
+sub cantidadDeParametros{
+	$cantidad = $#ARGV;
+	return $cantidad;
+}
+
+sub parametrosValidos{
+	#acepta 1 o 2 parametros
+	if ($#ARGV < 0  or $#ARGV > 1) {
+		imprimir("todo mal");
+		return false;
+	}
+
+	imprimir("todo bien");
+	return true;
+	
+}
+
+
+sub logger{
+	system("Glog.sh perl log INFO");
+}
+
+sub main{
+	logger();
+	if (!parametrosValidos()) {
+		imprimir("algo salio mal");
+		ayuda();
+		exit;
+	}
+
+	$parametroValido = false;
+	#primer parametro debe ser opciones
+	$opcion = $ARGV[0];
+	if($opcion eq "-a"){
+		imprimir("parametro -a");
+		ayuda();
+		$parametroValido = true;
+	}
+	if($opcion eq "-c"){
+		imprimir("parametro -c");
+		$parametroValido = true;
+	}
+	if($opcion eq "-i"){
+		imprimir("parametro -i");
+		$parametroValido = true;
+	}
+	if($opcion eq "-e"){
+		imprimir("parametro -e");
+		$parametroValido = true;
+	}
+	if($opcion eq "-cg"){
+		imprimir("parametro -cg");
+		$parametroValido = true;
+	}
+	if($opcion eq "-ig"){
+		imprimir("parametro -ig");
+		$parametroValido = true;
+	}
+	if($opcion eq "-eg"){
+		imprimir("parametro -eg");
+		$parametroValido = true;
+	}
+
+	
+
+	
+}
