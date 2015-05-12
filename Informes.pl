cf3f9811833e69fab0f46dd12fe05a4c8dfa7451
rsolotun <rsolotun@gmail.com>
2015-05-12 01:38:50 -0300

    Informes.pl terminado

--- Informes.pl
+++ Informes.pl
@@ -1,6 +1,7 @@
 #! /usr/bin/perl -w
 
 use Getopt::Long;
+use List::MoreUtils qw(uniq);
 
 use constant false => 0;
 use constant true  => 1;
@@ -12,6 +13,8 @@ my $GRUPO = $ENV{'GRUPO'};
 ## Levanto a memoria los archivos de gestiones y emisores
 my %hash_gestiones = cargarHashGestiones();
 my %hash_emisores = cargarHashEmisores();
+my %hash_normas = cargarHashNormas();
+my %hash_gestionesOrdenadas = cargarHashGestionesOrdenadas();
 main();
 
 
@@ -28,12 +31,12 @@ sub ayuda{
 	imprimir("\ts:\t\tSalir.");
 }
 sub imprimir {
-    $a = shift."\n";
+    my $a = shift."\n";
     print $a;
 }
 
 sub cantidadDeParametros{
-	$cantidad = $#ARGV;
+	my $cantidad = $#ARGV;
 	return $cantidad;
 }
 
@@ -83,31 +86,27 @@ sub mostrarMenuPrencipal{
 sub levantarProcesoOpcion{
 
 	#primer parametro debe ser opciones
-	$opcion = shift;
+	my $opcion = shift;
 	if($opcion eq "a"){
 		imprimir("parametro a");
 		ayuda();
 	}
 	if($opcion eq "c"){
-		imprimir("parametro c");
 		consulta(false);
 	}
 	if($opcion eq "gc"){
-		imprimir("parametro gc");
 		consulta(true);
 	}
 	if($opcion eq "e"){
-		imprimir("parametro e");
+		estadistica(false);
 	}
 	if($opcion eq "ge"){
-		imprimir("parametro ge");
+		estadistica(true);
 	}
 	if($opcion eq "gi"){
-		imprimir("parametro gi");
 		informe(true);
 	}
 	if($opcion eq "i"){
-		imprimir("parametro i");
 		informe(false);
 	}
 	if($opcion eq "s"){
@@ -140,7 +139,7 @@ sub in_array {
 sub menuPrincipal{
 	ayuda();
 	imprimir("Ingrese una opción: ");
-	$entrada = <STDIN>;
+	my $entrada = <STDIN>;
 	chomp($entrada);	
 	
 	#Loop infinito hasta que se ingrese opcion valida
@@ -155,7 +154,7 @@ sub menuPrincipal{
 }
 
 sub consulta{
-	$conGuardar= shift;
+	my $conGuardar= shift;
 	my $palabra_clave = ingresarPalabraClave();
 	my @registros = filtrarRegistrosProtocolizados();
 	my @registrosAOrdenar;
@@ -174,26 +173,123 @@ sub consulta{
 		imprimir($extracto);
 	}
 	if($conGuardar){
-		$nombreArchivo = nombreArchivoConsultas();
+		my $nombreArchivo = nombreArchivoConsultas();
 		open(my $fh, '>', $nombreArchivo) || die "No puede abrirse el archivo $nombreArchivo\n";
 		foreach $reg (@regisrosOrdenados){
 			my ($fechaNorma,$nroNorma,$anioNorma,$causante,$extracto,$idReg,$codGestion,$codNorma,$codEmisor) = (split ";", $reg)[2,3,4,5,6,11,12,13,14];
 			print $fh $codNorma.";".$hash_emisores{$codEmisor}.";".$codEmisor.";".$nroNorma.";".$anioNorma.";".$codGestion.";".$fechaNorma.";".$causante.";".$extracto.";".$idReg."\n";
 		}
 		close $fh;
+		imprimir("Consulta grabada en $nombreArchivo");
 	}
 }
 
 sub informe{
-	$conGuardar= shift;
-	$palabra_clave = ingresarPalabraClave();
-	pedirFiltros($filtro_tipo_norma, $filtro_anios, $filtro_nro_norma, $filtro_gestion, $filtro_emisor);
+	my $conGuardar= shift;
+	my @filtroArchivos = ingresarFiltroArchivos();
+	my $palabra_clave = ingresarPalabraClave();
+	my @registros = filtrarRegistrosConsultados(@filtroArchivos);
+	my @registrosUnicos = uniq(@registros);
+	my @registrosAOrdenar;
+	foreach $reg (@registros){
+		my ($causante,$extracto) = (split ";", $reg)[7,8];
+		my $peso=calcularPesoRegistro($causante,$extracto,$palabra_clave);
+		my $nuevoReg = $peso.";".$reg;
+		push(@registrosAOrdenar,$nuevoReg);
+	}
+	my @regOrdPorFecha = sort{((split "/", (split ";", $b)[7])[2]).((split "/", (split ";", $b)[7])[1]).((split "/", (split ";", $b)[7])[0]) cmp ((split "/", (split ";", $a)[7])[2]).((split "/", (split ";", $a)[7])[1]).((split "/", (split ";", $a)[7])[0])} @registrosAOrdenar;
+	my @regisrosOrdenados = sort{(split ";", $b)[0] <=> (split ";", $a)[0]} @regOrdPorFecha;
+	foreach $reg (@regisrosOrdenados){
+		my ($peso,$codNorma,$emisor,$codEmisor,$nroNorma,$anioNorma,$codGestion,$fechaNorma,$causante,$extracto,$idReg) = (split ";", $reg);
+		imprimir($codNorma." ".$hash_emisores{$codEmisor}."(".$codEmisor.") ".$nroNorma."/".$anioNorma." ".$codGestion." ".$fechaNorma." ".$peso);
+		imprimir($causante);
+		imprimir($extracto);
+	}
 	if($conGuardar){
-		imprimir("guardando");
-	}else{
-		imprimir("sin Guardar");
+		my $nombreArchivo = nombreArchivoInformes();
+		open(my $fh, '>', $nombreArchivo) || die "No puede abrirse el archivo $nombreArchivo\n";
+		foreach $reg (@regisrosOrdenados){
+			my ($peso,$codNorma,$emisor,$codEmisor,$nroNorma,$anioNorma,$codGestion,$fechaNorma,$causante,$extracto,$idReg) = (split ";", $reg);
+			print $fh $codNorma.";".$hash_emisores{$codEmisor}.";".$codEmisor.";".$nroNorma.";".$anioNorma.";".$codGestion.";".$fechaNorma.";".$causante.";".$extracto.";".$idReg."\n";
+		}
+		close $fh;
+		imprimir("Informe grabado en $nombreArchivo");
 	}
 
+}
+
+sub estadistica{
+	my $conGuardar= shift;
+	my @registros = filtrarRegistrosProtocolizadosParaEstadisticas();
+	my @regisrosOrdenados = sort{((split "/", (split ";", $a)[1])[2]).((split "/", (split ";", $a)[1])[1]).((split "/", (split ";", $a)[1])[0]) cmp ((split "/", (split ";", $b)[1])[2]).((split "/", (split ";", $b)[1])[1]).((split "/", (split ";", $b)[1])[0])} @registros;
+	my @emisores;
+	my $cantResoluciones = 0;
+	my $cantDisposiciones = 0;
+	my $cantConvenios = 0;
+	my $gestionActual;
+	my $anioActual;
+	my %sumarizacionResoluciones;
+	my %sumarizacionDisposiciones;
+	my %sumarizacionConvenios;
+	my %sumarizacionEmisores;
+	foreach $reg (@regisrosOrdenados){
+		my ($fechaNorma,$anioNorma,$codGestion,$codNorma,$codEmisor) = (split ";", $reg)[2,3,11,12,13];
+		if ( not exists $sumarizacionEmisores{ $codGestion.";".$anioNorma } ){ 
+			$cantResoluciones = 0;
+			$cantDisposiciones = 0;
+			$cantConvenios = 0;
+			@emisores = ();
+		}
+		if ( $hash_norma{$codNorma} eq "RESOLUCION" ){$cantResoluciones++;}
+		if ( $hash_norma{$codNorma} eq "DISPOSICION" ){$cantDisposiciones++;}
+		if ( $hash_norma{$codNorma} eq "CONVENIO" ){$cantConvenios++;}
+		my @aux = @emisores;
+		push(@aux, $hash_emisores{$codEmisor});
+		@emisores = uniq(@aux);
+		$sumarizacionResoluciones{ $codGestion.";".$anioNorma } = $cantResoluciones;
+		$sumarizacionDisposiciones{ $codGestion.";".$anioNorma } = $cantDisposiciones;
+		$sumarizacionConvenios{ $codGestion.";".$anioNorma } = $cantConvenios;
+		$sumarizacionEmisores{ $codGestion.";".$anioNorma } = [@emisores];
+	}
+
+	my @hash_claves_ordenadas = ordenarClavesEstadistica(keys %sumarizacionEmisores);
+	foreach my $key (@hash_claves_ordenadas) {
+		print ("Gestion: ".$hash_gestiones{(split ";", $key)[0]}." Año: ".(split ";", $key)[1]." Emisores: ");
+		my $listaEmisores = "";
+		foreach my $emisor (@{$sumarizacionEmisores{$key}}){
+			$listaEmisores=$listaEmisores.", ".$emisor;
+		}
+		$listaEmisores=substr($listaEmisores,2);
+		print $listaEmisores=$listaEmisores."\n";
+		imprimir("Cantidad de resoluciones: ".$sumarizacionResoluciones{$key});
+		imprimir("Cantidad de disposiciones: ".$sumarizacionDisposiciones{$key});
+		imprimir("Cantidad de convenios: ".$sumarizacionConvenios{$key});
+	}
+	if($conGuardar){
+		my $nombreArchivo = nombreArchivoEstadisticas();
+		open(my $fh, '>', $nombreArchivo) || die "No puede abrirse el archivo $nombreArchivo\n";
+		foreach my $key (@hash_claves_ordenadas) {
+			print $fh ("Gestion: ".$hash_gestiones{(split ";", $key)[0]}." Año: ".(split ";", $key)[1]." Emisores: ");
+			my $listaEmisores = "";
+			foreach my $emisor (@{$sumarizacionEmisores{$key}}){
+				$listaEmisores=$listaEmisores.", ".$emisor;
+			}
+			$listaEmisores=substr($listaEmisores,2);
+			print $fh $listaEmisores=$listaEmisores."\n";
+			print $fh "Cantidad de resoluciones: ".$sumarizacionResoluciones{$key}."\n";
+			print $fh "Cantidad de disposiciones: ".$sumarizacionDisposiciones{$key}."\n";
+			print $fh "Cantidad de convenios: ".$sumarizacionConvenios{$key}."\n";
+		}
+		close $fh;
+		imprimir("Consulta grabada en $nombreArchivo");
+	}
+}
+
+sub ordenarClavesEstadistica(){
+	my @claves=@_;
+	my @claves_ordenadas_por_anio = sort{ (split ";", $a)[1] <=> (split ";", $b)[1] } @claves;
+	my @claves_ordenadas = sort{ $hash_gestionesOrdenadas{(split ";", $a)[0]} <=> $hash_gestionesOrdenadas{(split ";", $b)[0]} } @claves_ordenadas_por_anio;
+	return (@claves_ordenadas);
 }
 
 sub calcularPesoRegistro{
@@ -230,20 +326,41 @@ sub pedirFiltros{
 	$_[4] = $filtro_emisor;
 }
 
+sub pedirFiltrosParaEstadisticas{
+	my ($filtro_anios, $filtro_gestion) = @_;	
+	$filtro_anios = ingresarFiltroPorAnio(); 	 
+	$filtro_gestion = ingresarFiltroPorGestion();
+
+
+	if ( ($filtro_anios eq "") && ($filtro_gestion eq "") ){
+		imprimir("Se tomarán todas las posibles combinaciones de años y gestiones.");
+	}
+	$_[0] = $filtro_anios;
+	$_[1] = $filtro_gestion;
+}
+
 sub ingresarPalabraClave{
 	print "Ingrese palabra clave: ";
-	$palabra_clave = <STDIN>;
+	my $palabra_clave = <STDIN>;
 	chomp($palabra_clave);
 	return $palabra_clave;
 }
 
+sub ingresarFiltroArchivos{
+	print "Ingrese la lista de archivos resultado separados por coma: ";
+	my $archivos = <STDIN>;
+	chomp($archivos);
+	@listaArchivos = (split",", $archivos);
+	return @listaArchivos;
+}
+
 sub ingresarFiltroPorTipoNorma{
 	print "Ingrese tipo de norma para filtrar: ";
-	$tipo_norma = <STDIN>;
+	my $tipo_norma = <STDIN>;
 	chomp($tipo_norma);
-	$tipo_norma_aux = $tipo_norma;
+	my $tipo_norma_aux = $tipo_norma;
 	$tipo_norma_aux =~ s/\s//g;	
-	$esValido = 0;
+	my $esValido = 0;
 	
 	# Validaciones
 	while ( $esValido == 0 ){	
@@ -268,7 +385,7 @@ sub ingresarFiltroPorTipoNorma{
 sub ingresarFiltroPorAnio{
 
 	print "Ingrese rango de años para filtrar con el formato ####-####: ";
-	$rango_anios = <STDIN>;
+	my $rango_anios = <STDIN>;
 	chomp($rango_anios);
 	
 	$rango_anios = validarRangoAnios($rango_anios);		
@@ -277,8 +394,8 @@ sub ingresarFiltroPorAnio{
 }
 
 sub validarRangoAnios{
-	$rango_anios = $_[0];
-	$esValido = 0;
+	my $rango_anios = $_[0];
+	my $esValido = 0;
 	
 	while ( $esValido == 0 ){
 		if( ( ( length($rango_anios) == 9 ) && ( $rango_anios =~ /[0-9]{4}.[0-9]{4}/ ) ) || ( length($rango_anios) == 0 )){
@@ -295,9 +412,9 @@ sub validarRangoAnios{
 sub ingresarFiltroPorNroNorma{
 	
 	print "Ingrese rango de números de norma para filtrar con el formato ####-####: ";
-	$rango_nro_norma = <STDIN>;
+	my $rango_nro_norma = <STDIN>;
 	chomp($rango_nro_norma);
-	$esValido = 0;
+	my $esValido = 0;
 
 	while ( $esValido == 0 ){
 		if ( ( ($rango_nro_norma =~ /[0-9]{4}.[0-9]{4}/) && ( length($rango_nro_norma) == 9 ) ) || (length($rango_nro_norma) == 0) ){				
@@ -337,9 +454,9 @@ sub ingresarFiltroPorGestion{
 
 sub ingresarFiltroPorEmisor{
 	print "Ingrese un emisor para filtrar: ";
-	$emisor = <STDIN>;
+	my $emisor = <STDIN>;
 	chomp($emisor);	
-	$esValido = 0;
+	my $esValido = 0;
 	
 	# Validaciones
 	while ( $esValido == 0 ){	
@@ -365,21 +482,21 @@ sub cargarHashArchivosProtocolizados{
 	my @archivosProtocolizados = cargarArchivosProtocolizados($GRUPO.$PROCDIR);
 	foreach my $archivo (@archivosProtocolizados){
 		chomp($archivo);
-		$auxArchivo=$archivo;
-		$aux= () = $auxArchivo =~ /\//g;
+		my $auxArchivo=$archivo;
+		my $aux= () = $auxArchivo =~ /\//g;
 		while ( $aux > 1 ){
   			substr($auxArchivo, 0, index($auxArchivo, '/')+1) = '';
 			$aux= () = $auxArchivo =~ /\//g;
 		}
 		my ($gestion, $nombreArchivo)=(split "\/", $auxArchivo);
 		my ($anioNorma, $codNorma)=(split /\./, $nombreArchivo);
-		$hash_archivos{ "$gestion;$anioNorma;$codNorma" } = "$archivo";
+		$hash_archivos{ $gestion.";".$anioNorma.";".$codNorma } = $archivo;
 	}
 	return %hash_archivos;
 }
 
 sub cargarArchivosProtocolizados{
-	$directorio_padre = $_[0];
+	my $directorio_padre = $_[0];
 	my @rutas;
 	opendir(DP, $directorio_padre) || die "No puede abrirse el directorio $directorio_padre\n";
 
@@ -402,8 +519,39 @@ sub cargarArchivosProtocolizados{
 	return (@rutas);
 }
 
+sub cargarArchivosDeConsultas{
+	my @filtroArchivos = @_;
+	my $dir = $GRUPO.$INFODIR;
+	my @archivos = cargarArchivos($dir);
+	my @archivos_consultas;
+
+	if ( scalar(@filtroArchivos) == 0){
+		foreach my $archivo (@archivos){
+			my $aux = $archivo;
+			chomp($archivo);
+			substr($archivo, 0, rindex($archivo, '/')+1) = '';
+			if ( $archivo =~ /resultados/g ){
+				push(@archivos_consultas, $aux);
+			}
+		}	
+	}else{
+		foreach my $archivo (@archivos){
+			my $aux = $archivo;
+			chomp($archivo);
+			substr($archivo, 0, rindex($archivo, '/')+1) = '';
+			foreach my $archivoFiltro (@filtroArchivos){
+				if ( $archivo eq $archivoFiltro ){
+					push(@archivos_consultas, $aux);
+				}
+			}
+		}
+	}
+	
+	return (@archivos_consultas);
+}
+
 sub cargarArchivos{
-	$directorio_padre = $_[0];
+	my $directorio_padre = $_[0];
 	my @rutas;
 	opendir(DP, $directorio_padre) || die "No puede abrirse el directorio $directorio_padre\n";
 
@@ -428,6 +576,20 @@ sub cargarHashGestiones{
 	return (%hash_gestiones);
 }
 
+sub cargarHashGestionesOrdenadas{
+	my %hash_gestiones;
+	my $direccion_gestiones = $GRUPO.$MAEDIR."/gestiones.mae";
+	open (FILE, $direccion_gestiones) || die "Error al abrir archivo de gestiones.mae";
+	my $orden = 0;
+	while(my $reg = <FILE>){
+		my ($cod_gestion) = (split";", $reg)[0];
+		$hash_gestiones{$cod_gestion} = $orden;
+		$orden++;
+	}
+	close(FILE);
+	return (%hash_gestiones);
+}
+
 sub cargarHashEmisores{
 	my %hash_emisores;
 	my $direccion_emisores = $GRUPO.$MAEDIR."/emisores.mae";
@@ -440,32 +602,45 @@ sub cargarHashEmisores{
 	return (%hash_emisores);
 }
 
+sub cargarHashNormas{
+	my %hash_normas;
+	my $direccion_normas = $GRUPO.$MAEDIR."/normas.mae";
+	open(FILE, $direccion_normas) || die "Error al abrir archivo de normas.mae";
+	while(my $reg = <FILE>){
+		my ($cod_norma, $nombre_norma) = (split";", $reg)[0,1];
+		$hash_norma{$cod_norma} = $nombre_norma;
+	}
+	close(FILE);
+	return (%hash_normas);
+}
+
 sub filtrarRegistrosConsultados{
-	my %hash_archivos = shift;
+	my @archivos = cargarArchivosDeConsultas(@_);
 	my @regsistrosValidos;
-	my $palabra_clave = ingresarPalabraClave();
 	pedirFiltros($filtro_tipo_norma, $filtro_anios, $filtro_nro_norma, $filtro_gestion, $filtro_emisor);
-	for my $key ( keys %hash_archivos ) {
-		my ($gestion, $anioNorma, $codNorma) = (split ";" , $key);
-		if($filtro_gestion ne ""){
-			if($filtro_gestion ne $gestion){
-				next;
-			}
-		}
-		if($filtro_anios ne ""){
-			my ($filtroDesde, $filtroHasta) = (split "-" , $filtro_anios);
-			if($filtroDesde > $anioNorma || $filtroHasta < $anioNorma){
-				next;
-			}
-		}
-		if($filtro_tipo_norma ne ""){
-			if($filtro_tipo_norma ne $codNorma){
-				next;
-			}		
-		}
-		open (FILE, "$hash_archivos{$key}") or die "Falla al abrir $hash_archivos{$key}";
+	foreach my $archivo ( @archivos ) {
+		open (FILE, $archivo) or die "Falla al abrir $archivo";
 		while ($reg = <FILE>){
-			my($codEmisor, $nroNorma) = (split ";", $reg)[13,2];
+			chomp($reg);
+			my($codNorma, $codEmisor, $nroNorma, $anioNorma, $gestion) = (split ";", $reg)[0,2,3,4,5];
+			if($filtro_gestion ne ""){
+				my $descGestion = $hash_gestiones{$gestion};
+				my $aux = lc($filtro_gestion);
+				if( lc($descGestion) !~ /$aux/g){
+					next;
+				}
+			}
+			if($filtro_anios ne ""){
+				my ($filtroDesde, $filtroHasta) = (split "-" , $filtro_anios);
+				if($filtroDesde > $anioNorma || $filtroHasta < $anioNorma){
+					next;
+				}
+			}
+			if($filtro_tipo_norma ne ""){
+				if(lc($filtro_tipo_norma) ne lc($codNorma)){
+					next;
+				}		
+			}
 			if($filtro_nro_norma ne ""){
 				my ($filtroDesde, $filtroHasta) = (split "-" , $filtro_nro_norma);
 				if($filtroDesde > $nroNorma || $filtroHasta < $nroNorma){
@@ -473,22 +648,25 @@ sub filtrarRegistrosConsultados{
 				}		
 			}
 			if($filtro_emisor ne ""){
-				if($filtro_emisor ne $codEmisor){
+				my $descEmisor = $hash_emisores{$codEmisor};
+				my $aux = lc($filtro_emisor);
+				if( lc($descEmisor) !~ /$aux/g ){
 					next;
 				}		
 			}
 			push(@regsistrosValidos, $reg);
 		} 
+		close(FILE);
 	}
 	return (@regsistrosValidos);
 }
 
 sub filtrarRegistrosProtocolizados{
-	#hash_archivos; key="$gestion;$anioNorma;$codNorma"; value="$archivo"
+	#hash_archivos; key=$gestion.";".$anioNorma.";".$codNorma; value=$archivo
 	my %hash_archivos = cargarHashArchivosProtocolizados();
 	my @regsistrosValidos;
 	pedirFiltros($filtro_tipo_norma, $filtro_anios, $filtro_nro_norma, $filtro_gestion, $filtro_emisor);
-	for my $key ( keys %hash_archivos ) {
+	foreach my $key ( keys %hash_archivos ) {
 		my ($gestion, $anioNorma, $codNorma) = (split ";" , $key);
 		if($filtro_gestion ne ""){
 			my $descGestion = $hash_gestiones{$gestion};
@@ -508,7 +686,7 @@ sub filtrarRegistrosProtocolizados{
 				next;
 			}		
 		}
-		open (FILE, "$hash_archivos{$key}") or die "Falla al abrir $hash_archivos{$key}";
+		open (FILE, $hash_archivos{$key}) or die "Falla al abrir $hash_archivos{$key}";
 		while ($reg = <FILE>){
 			chomp($reg);
 			my($codEmisor, $nroNorma) = (split ";", $reg)[13,2];
@@ -526,7 +704,38 @@ sub filtrarRegistrosProtocolizados{
 				}		
 			}
 			push(@regsistrosValidos, $reg);
-		} 
+		}
+		close(FILE);
+	}
+	return (@regsistrosValidos);
+}
+
+sub filtrarRegistrosProtocolizadosParaEstadisticas{
+	#hash_archivos; key=$gestion.";".$anioNorma.";".$codNorma; value=$archivo
+	my %hash_archivos = cargarHashArchivosProtocolizados();
+	my @regsistrosValidos;
+	pedirFiltrosParaEstadisticas($filtro_anios, $filtro_gestion);
+	foreach my $key ( keys %hash_archivos ) {
+		my ($gestion, $anioNorma, $codNorma) = (split ";" , $key);
+		if($filtro_gestion ne ""){
+			my $descGestion = $hash_gestiones{$gestion};
+			my $aux = lc($filtro_gestion);
+			if( lc($descGestion) !~ /$aux/g){
+				next;
+			}
+		}
+		if($filtro_anios ne ""){
+			my ($filtroDesde, $filtroHasta) = (split "-" , $filtro_anios);
+			if($filtroDesde > $anioNorma || $filtroHasta < $anioNorma){
+				next;
+			}
+		}
+		open (FILE, $hash_archivos{$key}) or die "Falla al abrir $hash_archivos{$key}";
+		while ($reg = <FILE>){
+			chomp($reg);
+			push(@regsistrosValidos, $reg);
+		}
+		close(FILE);
 	}
 	return (@regsistrosValidos);
 }
@@ -535,12 +744,9 @@ sub nombreArchivoConsultas{
 	my $dir = $GRUPO.$INFODIR;
 	my @archivos = cargarArchivos($dir);
 	
-	$i = 0;
-	$cantArchivos =scalar @archivos;
-	$ultimoNroUsado=0;
+	my $ultimoNroUsado=0;
 
-	while ($i < $cantArchivos){
-		$archivo = $archivos[$i];
+	foreach my $archivo (@archivos){
 		chomp($archivo);
 		substr($archivo, 0, rindex($archivo, '/')+1) = '';
 		if ( $archivo =~ /resultados/g ){
@@ -549,10 +755,53 @@ sub nombreArchivoConsultas{
 				$ultimoNroUsado = $nro;
 			}
 		}
-		$i++;
 	}	
-	$sigNro = $ultimoNroUsado + 1;
-	$result = sprintf('%03d',$sigNro);
-	$nombreArchivo = $GRUPO.$INFODIR."/resultados_".$result;
+	my $sigNro = $ultimoNroUsado + 1;
+	my $result = sprintf('%03d',$sigNro);
+	my $nombreArchivo = $GRUPO.$INFODIR."/resultados_".$result;
+	return $nombreArchivo;
+}
+
+sub nombreArchivoInformes{
+	my $dir = $GRUPO.$INFODIR;
+	my @archivos = cargarArchivos($dir);
+	
+	my $ultimoNroUsado=0;
+
+	foreach my $archivo (@archivos){
+		chomp($archivo);
+		substr($archivo, 0, rindex($archivo, '/')+1) = '';
+		if ( $archivo =~ /informe/g ){
+			$nro=(split "_", $archivo)[1];
+			if ($ultimoNroUsado < $nro){
+				$ultimoNroUsado = $nro;
+			}
+		}
+	}	
+	my $sigNro = $ultimoNroUsado + 1;
+	my $result = sprintf('%03d',$sigNro);
+	my $nombreArchivo = $GRUPO.$INFODIR."/informe_".$result;
+	return $nombreArchivo;
+}
+
+sub nombreArchivoEstadisticas{
+	my $dir = $GRUPO.$INFODIR;
+	my @archivos = cargarArchivos($dir);
+	
+	my $ultimoNroUsado=0;
+
+	foreach my $archivo (@archivos){
+		chomp($archivo);
+		substr($archivo, 0, rindex($archivo, '/')+1) = '';
+		if ( $archivo =~ /estadistica/g ){
+			$nro=(split "_", $archivo)[1];
+			if ($ultimoNroUsado < $nro){
+				$ultimoNroUsado = $nro;
+			}
+		}
+	}	
+	my $sigNro = $ultimoNroUsado + 1;
+	my $result = sprintf('%03d',$sigNro);
+	my $nombreArchivo = $GRUPO.$INFODIR."/estadistica_".$result;
 	return $nombreArchivo;
 }
